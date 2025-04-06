4c2c422b47d6ee6860e9c32a88a55ade66322ae7
Ayush9545 <ayushgotefode@gmail.com>
2025-04-06 19:56:10 +0530

    This is the syntax of function

---
+++ syntax_of_function.java
@@ -0,0 +1,8 @@
+public class syntax_of_function{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 10;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
